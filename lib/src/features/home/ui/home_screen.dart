c28f34935da8f972f6a0953cf081bdb76c466704
Dmitry <puls_2@mail.ru>
2025-10-20 12:23:35 +0300

    Prac 4 done

--- lib/src/features/home/ui/home_screen.dart
+++ lib/src/features/home/ui/home_screen.dart
@@ -13,6 +13,40 @@ class HomeScreenState extends State<HomeScreen> {
 
   String imageName = "assets/images/patterns/adapter_image.png";
 
+  final images = const [
+    "assets/images/patterns/adapter_image.png",
+    "assets/images/patterns/mvvm_image.jpeg",
+    "assets/images/patterns/block_image.png",
+    "assets/images/patterns/adapter_image.png"
+  ];
+
+  final patterns = [
+    {
+      'number': '1',
+      'title': 'MVC (Model-View-Controller)',
+      'description': 'Разделение на модель, представление и контроллер',
+      'image_url': 'assets/images/patterns/mvc_image.jpg'
+    },
+    {
+      'number': '2',
+      'title': 'MVVM (Model-View-ViewModel)',
+      'description': 'Использование ViewModel для связывания данных',
+      'image_url': 'assets/images/patterns/mvvm_image.jpeg'
+    },
+    {
+      'number': '3',
+      'title': 'BLoC (Business Logic Component)',
+      'description': 'Управление состоянием через потоки и события',
+      'image_url': 'assets/images/patterns/block_image.png'
+    },
+    {
+      'number': '4',
+      'title': 'Adapter',
+      'description': 'Использование несовместимых типов данных посредством приведения одного к другому',
+      'image_url': 'assets/images/patterns/adapter_image.png'
+    },
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,20 +60,9 @@ class HomeScreenState extends State<HomeScreen> {
 
               _buildSoftwareCard(),
 
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Expanded(
-                    flex: 2,
-                    child: _buildImageSection(),
-                  ),
-                  const SizedBox(width: 8),
-                  Expanded(
-                    flex: 2,
-                    child: _buildPatternsList(),
-                  ),
-                ],
-              ),
+              _buildImageSection(),
+
+              _buildPatternsList(),
 
               _buildDeveloperInfo(),
             ],
@@ -132,169 +155,99 @@ class HomeScreenState extends State<HomeScreen> {
   }
 
   Widget _buildImageSection() {
-    return Container(
+    return SizedBox(
       height: 200,
-      width: 200,
-      padding: EdgeInsets.all(16),
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(12),
-          gradient: LinearGradient(
-            begin: Alignment.bottomCenter,
-            end: Alignment.topCenter,
-            colors: [
-              Colors.black.withValues(alpha: 0.6),
-              Colors.transparent,
-            ],
-          ),
-        ),
-        child: Center(
-          child: Image.asset(
-            imageName,
-            fit: BoxFit.cover,
-          ),
-        ),
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: images.length,
+        itemBuilder: (context, index) {
+          return Container(
+              width: 200,
+              height: 200,
+              margin: EdgeInsets.only(right: 12, left: 12),
+              decoration: BoxDecoration(
+                  color: Colors.blueGrey,
+                  borderRadius: BorderRadius.circular(20)
+              ),
+              child: Image.asset(images[index])
+          );
+        },
       ),
     );
   }
+
 
   Widget _buildPatternsList() {
-    final patterns = [
-      {
-        'number': '1',
-        'title': 'MVC (Model-View-Controller)',
-        'description': 'Разделение на модель, представление и контроллер',
-        'image_url': 'assets/images/patterns/mvc_image.jpg'
-      },
-      {
-        'number': '2',
-        'title': 'MVVM (Model-View-ViewModel)',
-        'description': 'Использование ViewModel для связывания данных',
-        'image_url': 'assets/images/patterns/mvvm_image.jpeg'
-      },
-      {
-        'number': '3',
-        'title': 'BLoC (Business Logic Component)',
-        'description': 'Управление состоянием через потоки и события',
-        'image_url': 'assets/images/patterns/block_image.png'
-      },
-      {
-        'number': '4',
-        'title': 'Adapter',
-        'description': 'Использование несовместимых типов данных посредством приведения одного к другому',
-        'image_url': 'assets/images/patterns/adapter_image.png'
-      },
-    ];
-
-    return Container(
-      height: 600,
-      padding: EdgeInsetsGeometry.all(16),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(12),
-        gradient: LinearGradient(
-          begin: Alignment.bottomCenter,
-          end: Alignment.topCenter,
-          colors: [
-            Colors.black.withValues(alpha: 0.6),
-            Colors.transparent,
-          ],
-        ),
-      ),
-      child: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'Популярные паттерны:',
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
-                color: Colors.blue[800],
-              ),
-            ),
-            const SizedBox(height: 16),
-            ...patterns.map((pattern) => _buildPatternItem(
-                pattern['number']!,
-                pattern['title']!,
-                pattern['description']!,
-                pattern['image_url']!
-            )),
-          ],
-        ),
-      )
-    );
-  }
-
-  Widget _buildPatternItem(String number, String title, String description, String imageUrl) {
-    return InkWell(
-      onTap: () {
-        _handleTap(imageUrl);
-      },
-      child: Container(
-        margin: const EdgeInsets.only(bottom: 12),
-        padding: const EdgeInsets.all(16),
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(8),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black.withOpacity(0.05),
-              blurRadius: 4,
-              offset: const Offset(0, 1),
-            ),
-          ],
-          border: Border.all(
-            color: Colors.blue.withOpacity(0.1),
-            width: 1,
-          ),
-        ),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              width: 30,
-              height: 30,
+    return SizedBox(
+      height: 200,
+      child: ListView.builder(
+        itemCount: patterns.length,
+        itemBuilder: (context, index) {
+          return InkWell(
+            onTap: () {
+              _handleTap(patterns[index]['title']!);
+            },
+            child: Container(
+              margin: const EdgeInsets.only(bottom: 12),
+              padding: const EdgeInsets.all(16),
               decoration: BoxDecoration(
-                color: Colors.blue[500],
-                shape: BoxShape.circle,
-              ),
-              child: Center(
-                child: Text(
-                  number,
-                  style: const TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 10,
-                  ),
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(8),
+                border: Border.all(
+                  color: Colors.blue.withOpacity(0.1),
+                  width: 1,
                 ),
               ),
-            ),
-            const SizedBox(width: 12),
-            Expanded(
-              child: Column(
+              child: Row(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text(
-                    title,
-                    style: TextStyle(
-                      fontSize: 12,
-                      fontWeight: FontWeight.w600,
-                      color: Colors.blue[800],
+                  Container(
+                    width: 30,
+                    height: 30,
+                    decoration: BoxDecoration(
+                      color: Colors.blue[500],
+                      shape: BoxShape.circle,
+                    ),
+                    child: Center(
+                      child: Text(
+                        patterns[index]["number"]!,
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 10,
+                        ),
+                      ),
                     ),
                   ),
-                  const SizedBox(height: 4),
-                  Text(
-                    description,
-                    style: TextStyle(
-                      fontSize: 10,
-                      color: Colors.grey[600],
+                  const SizedBox(width: 12),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          patterns[index]["title"]!,
+                          style: TextStyle(
+                            fontSize: 12,
+                            fontWeight: FontWeight.w600,
+                            color: Colors.blue[800],
+                          ),
+                        ),
+                        const SizedBox(height: 4),
+                        Text(
+                          patterns[index]["description"]!,
+                          style: TextStyle(
+                            fontSize: 10,
+                            color: Colors.grey[600],
+                          ),
+                        ),
+                      ],
                     ),
                   ),
                 ],
               ),
             ),
-          ],
-        ),
+          );
+        },
       ),
     );
   }
@@ -334,9 +287,16 @@ class HomeScreenState extends State<HomeScreen> {
     );
   }
 
-  void _handleTap(String imageUrl){
-    setState(() {
-      imageName = imageUrl;
-    });
+  void _handleTap(String title){
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar( content: Text(title),
+        action: SnackBarAction(
+          label: 'UNDO',
+          onPressed: () {
+
+          },
+        ),
+        duration: const Duration(seconds: 3),
+      ), );
   }
 }
